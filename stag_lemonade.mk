41d5b2873c27bc6cec6372534dc6b883877c0b1e
vjspranav <pranavasri@live.in>
2021-12-21 18:00:46 +0530

    Use latest redfin fp

--- stag_lemonade.mk
+++ stag_lemonade.mk
@@ -35,6 +35,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus9 \
     PRODUCT_NAME=OnePlus9_EEA \
-    PRIVATE_BUILD_DESC="OnePlus9-user 11   release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 SQ1A.211205.008 7888514 release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:11/RKQ1.201105.002/2111112053:user/release-keys
+BUILD_FINGERPRINT := "google/redfin/redfin:11/SQ1A.211205.008/7888514:user/release-keys"
