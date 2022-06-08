176e9ebecd56a425a92920914f0ec2369fe23ef5
chandu078 <chandudyavanapelli03@gmail.com>
2022-06-08 05:55:41 +0000

    lemonade: Set default hotspot ssid by RRO
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -26,7 +26,8 @@ TARGET_OTA_ASSERT_DEVICE := lemonade,OnePlus9
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    OPlus9WifiOverlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
